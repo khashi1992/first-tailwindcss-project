20c56709538c98682b05160f798764fb60162d17
khashayar <46204004+khashi1992@users.noreply.github.com>
2024-06-12 17:27:17 +0330

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'logo': "url('../src/img/a.svg')",
+        'logo': "url('/img/a.svg')",
         'bg1': "url('../src/img/hero-2.jpg')",
         'bg2': "url('../src/img/hero-3.jpg')",
         'bg3': "url('../src/img/p.png')",
